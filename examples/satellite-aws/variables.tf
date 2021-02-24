44b7aa9dd9f1324f511b0b874b144d260e7eb857
Jake Kitchener <jakek@us.ibm.com>
2021-02-23 22:42:06 -0500

    remove sensitive for schematics

--- examples/satellite-aws/variables.tf
+++ examples/satellite-aws/variables.tf
@@ -12,13 +12,11 @@ variable "ibmcloud_api_key" {
 variable "aws_access_key" {
   description  = "AWS access key"
   type         = string
-  sensitive   = true
 }
 
 variable "aws_secret_key" {
   description  = "AWS secret key"
   type         = string
-  sensitive   = true
 }
 
 variable "ibm_region" {
